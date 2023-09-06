88fa1242b8818cd91f6d4fedb61b997ba8cd0838
Jake Stewart <jakey@snakey.local>
2023-09-06 14:24:27 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+recursive macros
+----------------
+
+hit `q` to start recording, and `q` to end recording.
+You can now hit `Q` to replay the macro.
+
+Or, hit `q` to start recording, `2q` to begin recording a recursive macro,
+`q` to end the recursive macro and continue recording the first macro,
+and hit `Q` to replay the recursive macro. Finally, hit `q` to stop recording
+the first macro.
+
+The default registers used are `qwerty`, so 6 levels deep is supported. set your own registers if you want further depth.
+
+### Setup
+
+```lua
+require("recursive-macros").setup()
+```
+
+or for more customisation:
+
+```lua
+require("recursive-macros").setup({
+    registers = "qwerty",
+    startMacro = "q",
+    recurseMacro = "2q",
+    replayMacro = "Q",
+})
+```
+
---
+++ recursive-macro/init.lua
@@ -0,0 +1,112 @@
+local function map(mapping, f)
+    vim.keymap.set({"n", "v"}, mapping, f, { expr = true })
+end
+
+local function unmap(mapping)
+    vim.keymap.set({"n", "v"}, mapping, function()
+        return ""
+    end, { expr = true })
+end
+
+local T = {
+    registers = "qwerty",
+    depth = 0,
+    macros = {},
+    startMacroKey = "q",
+    recurseMacroKey = "2q",
+    replayMacroKey = "Q",
+}
+
+function T.startMacro()
+    if vim.fn.exists("g:macro_keys") == 0 then
+        vim.cmd.echoerr("g:macro_keys is not set")
+        return
+    end
+    local keys
+    if T.depth < #T.registers then
+        keys = 'q' .. T.registers[T.depth + 1]
+    else
+        keys = 'q' .. T.registers[T.depth]
+    end
+    if T.depth == 0 then
+        T.macros = {""}
+        map("q", T.endMacro)
+        map(T.recurseMacroKey, T.startMacro)
+        map(T.replayMacroKey, T.replayMacro)
+    else
+        if T.depth < #T.registers then
+            if #T.macros < T.depth + 1 then
+                table.insert(T.macros, "")
+            else
+                T.macros[T.depth] = ""
+            end
+        end
+        vim.fn.feedkeys('q', 'nx')
+        local macro = vim.fn.getreg(T.registers[T.depth])
+
+        if vim.fn.endswith(macro, "2q") then
+            macro = string.sub(macro, 1, #macro - 2)
+        elseif vim.fn.endswith(macro, "2") then
+            macro = string.sub(macro, 1, #macro - 1)
+        end
+        T.macros[T.depth] = macro
+    end
+    if T.depth < #T.registers then
+        T.depth = T.depth + 1
+    end
+    return keys
+end
+
+function T.replayMacro()
+    vim.fn.feedkeys('q', 'nx')
+    local macro = vim.fn.getreg(T.registers[T.depth])
+    macro = string.sub(macro, 1, #macro - 1)
+    T.macros[T.depth] = T.macros[T.depth]
+        .. macro .. "@" .. T.registers[T.depth + 1]
+    vim.fn.feedkeys("@" .. T.registers[T.depth + 1], "m")
+    return "q" .. T.registers[T.depth]
+end
+
+function T.endMacro()
+    if T.depth == 0 then
+        return
+    end
+
+    vim.fn.feedkeys('q', 'nx')
+    local keys = ""
+    T.depth = T.depth - 1
+
+    T.macros[T.depth + 1] = T.macros[T.depth + 1]
+        .. vim.fn.getreg(T.registers[T.depth + 1])
+    vim.fn.setreg(T.registers[T.depth + 1], T.macros[T.depth + 1])
+
+    if T.depth == 0 then
+        vim.cmd.unmap(T.recurseMacroKey)
+        map(T.startMacroKey, T.startMacro)
+        map(T.replayMacroKey, "@q")
+    else
+        T.macros[T.depth] = T.macros[T.depth]
+            .. "@" .. T.registers[T.depth + 1]
+        keys = keys .. 'q' .. T.registers[T.depth]
+    end
+
+    return keys
+end
+
+function T.start()
+    map(T.startMacroKey, T.startMacro)
+    unmap(T.recurseMacroKey)
+    unmap(T.replayMacroKey)
+end
+
+
+local function setup(opts)
+    opts = opts or {}
+    T.registers = opts.registers or T.registers
+    T.startMacroKey = opts.startMacro or T.startMacroKey
+    T.recurseMacroKey = opts.recurseMacro or T.recurseMacroKey
+    T.replayMacroKey = opts.replayMacro or T.replayMacroKey
+    T.start()
+end
+
+return { setup = setup }
